61b352d2a3d3d97ebf3d81ee75d513cca1c795e8
vivek-2102 <vivek21iya@gmail.com>
2024-04-14 00:15:49 +0530

    port configuration

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from market import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=8080)
+
 
 
 
